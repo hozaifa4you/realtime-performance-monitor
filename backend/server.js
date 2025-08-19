0a72b4c88e26c4d0bdaa3f6a2d8ed425e35aa6c1
Hozaifa <hozaifabinmasud@gmail.com>
2025-08-19 19:12:40 +0600

    Setup react client

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,10 @@
 const cluster = require("cluster");
 const http = require("http");
 const { Server } = require("socket.io");
-const numCPUs = require("os").cpus().length;
 const { setupMaster, setupWorker } = require("@socket.io/sticky");
 const { createAdapter, setupPrimary } = require("@socket.io/cluster-adapter");
+
+const numCPUs = require("os").cpus().length;
 
 if (cluster.isPrimary) {
    console.log(`Master ${process.pid} is running`);
@@ -17,7 +18,7 @@ if (cluster.isPrimary) {
 
    setupPrimary();
 
-   httpServer.listen(3000);
+   httpServer.listen(3333);
 
    for (let i = 0; i < numCPUs.length; i++) {
       cluster.fork();
@@ -31,7 +32,9 @@ if (cluster.isPrimary) {
    console.log(`Worker ${process.pid} is running`);
 
    const httpServer = http.createServer();
-   const io = new Server(httpServer);
+   const io = new Server(httpServer, {
+      cors: { origin: ["http://localhost:5173"], credentials: true },
+   });
 
    io.adapter(createAdapter());
    setupWorker(io);
