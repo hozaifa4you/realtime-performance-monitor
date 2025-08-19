0c11ac93028f6d641fc4d4e1ddf58953a9460f3e
Hozaifa <hozaifabinmasud@gmail.com>
2025-08-19 14:20:46 +0600

    Get CPU performance

--- frontend-vanilla/index.js
+++ frontend-vanilla/index.js
@@ -1,25 +1,73 @@
 const { log } = require("console");
 const os = require("os");
 
-const osType = os.type() === "Darwin" ? `Mac (${os.type()})` : os.type();
-const uptime = os.uptime();
-const freeMem = os.freemem();
-const totalMem = os.totalmem();
-const usedMem = totalMem - freeMem;
-const memUseage = Math.floor((usedMem / totalMem) * 100) / 100;
-const cpus = os.cpus();
-const cpuType = cpus[0].model;
-const numCores = cpus.length;
-const cpuSpeed = cpus[0].speed;
+const cupAverage = () =>
+   new Promise((resolve, reject) => {
+      const cpus = os.cpus();
+      let idleMs = 0;
+      let totalMs = 0;
 
-log({
-   osType,
-   uptime,
-   freeMem,
-   totalMem,
-   usedMem,
-   memUseage,
-   cpuType,
-   numCores,
-   cpuSpeed,
-});
+      cpus.forEach((core) => {
+         for (mode in core.times) {
+            totalMs += core.times[mode];
+         }
+
+         idleMs += core.times.idle;
+      });
+
+      resolve({
+         idle: idleMs / cpus.length,
+         total: totalMs / cpus.length,
+      });
+   });
+
+const getCpuLoad = () =>
+   new Promise(async (resolve, reject) => {
+      const start = await cupAverage();
+
+      setTimeout(async () => {
+         const end = await cupAverage();
+
+         const idleDiff = end.idle - start.idle;
+         const totalDiff = end.total - start.total;
+
+         const percentOfCpu = 100 - Math.floor((100 * idleDiff) / totalDiff);
+
+         resolve(percentOfCpu);
+      }, 100);
+   });
+
+const performanceLoadData = () =>
+   new Promise(async (resolve, reject) => {
+      const osType = os.type() === "Darwin" ? `Mac (${os.type()})` : os.type();
+      const uptime = os.uptime();
+      const freeMem = os.freemem();
+      const totalMem = os.totalmem();
+      const usedMem = totalMem - freeMem;
+      const memUseage = Math.floor((usedMem / totalMem) * 100) / 100;
+      const cpus = os.cpus();
+      const cpuType = cpus[0].model;
+      const numCores = cpus.length;
+      const cpuSpeed = cpus[0].speed;
+      const cpuLoad = await getCpuLoad();
+
+      resolve({
+         osType,
+         uptime,
+         freeMem,
+         totalMem,
+         usedMem,
+         memUseage,
+         cpuType,
+         numCores,
+         cpuSpeed,
+         cpuLoad,
+      });
+   });
+
+const run = async () => {
+   const data = await performanceLoadData();
+   console.log(data);
+};
+
+run();
