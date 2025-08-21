4a345d75740d914ac41921d942648d1aa2f0934b
Hozaifa <hozaifabinmasud@email.com>
2025-08-21 08:51:03 +0600

    Configure shadcn ui

--- frontend/src/socketConnection.ts
+++ frontend/src/socketConnection.ts
@@ -1,8 +1,14 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:3333");
+const options = {
+   auth: {
+      token: "react-client-connection",
+   },
+};
+
+const socket = io("http://localhost:3333", options);
 socket.on("welcome", (message) => {
    console.log({ message });
 });
 
-export { socket };
+export default socket;
